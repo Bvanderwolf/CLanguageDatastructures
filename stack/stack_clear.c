43a14b4653ddd26b5483fd740faaa75dc3db3a22
Benjamin@WSL <benjaminvanderwolf@gmail.com>
2022-11-23 22:07:33 +0100

    add some test case meta data to stack_clear

--- stack/stack_clear.c
+++ stack/stack_clear.c
@@ -1,5 +1,11 @@
 #include "stack.h"
 
+// test case: (NULL, &free)		-> Segfault
+// test case: (lst[5], NULL)	-> Segfault
+// test case: (lst[0], &free)	-> []
+// test case: (lst[1], &free)	-> []
+// test case: (lst[5], &free)	-> []
+
 void	stack_clear(t_stack *stack, void (*del)(void *))
 {
 	llist_clear(&(stack->head), del);
